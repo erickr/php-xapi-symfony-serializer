9a5cc5779c5349958395897192947db611391760
Eric Krona <eric@krona.tm>
2023-10-31 13:24:57 +0100

    Added missing serilizers + php8 versions

--- spec/SerializerFactorySpec.php
+++ spec/SerializerFactorySpec.php
@@ -3,6 +3,7 @@
 namespace spec\Xabbuh\XApi\Serializer\Symfony;
 
 use spec\Xabbuh\XApi\Serializer\SerializerFactoryInterfaceSpec;
+use Xabbuh\XApi\Model\Activity;
 
 class SerializerFactorySpec extends SerializerFactoryInterfaceSpec
 {
